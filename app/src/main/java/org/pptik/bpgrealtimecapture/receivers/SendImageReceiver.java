c19c04bd25c040debb4c04b1a367ee77f3ef2205
Hendra Permana <hynra@yahoo.com>
2016-08-25 13:28:20 +0700

    Prepare : Move sending image logic to intent service. Separate process.

---
+++ app/src/main/java/org/pptik/bpgrealtimecapture/receivers/SendImageReceiver.java
@@ -0,0 +1,16 @@
+package org.pptik.bpgrealtimecapture.receivers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by hynra on 8/25/16.
+ */
+public class SendImageReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/org/pptik/bpgrealtimecapture/services/SendImageService.java
@@ -0,0 +1,19 @@
+package org.pptik.bpgrealtimecapture.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by hynra on 8/25/16.
+ */
+public class SendImageService extends IntentService {
+
+    public SendImageService() {
+        super("SchedulingService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
